cf76ff08168ce05df5c42bb88181db6143f3d8fb
akepativ <Laddu1988>
2020-04-05 23:39:51 +0100

    added add to cart button for collection items

--- src/components/collection-item/collection-item.component.jsx
+++ src/components/collection-item/collection-item.component.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 
+import CustomButton from "../custom-button/custom-button.component";
 import "./collection-item.styles.scss";
 
 const CollectionItem = ({ id, name, imageUrl, price }) => (
@@ -14,6 +15,7 @@ const CollectionItem = ({ id, name, imageUrl, price }) => (
       <span className="name">{name}</span>
       <span className="price">{price}</span>
     </div>
+    <CustomButton>Add to Cart</CustomButton>
   </div>
 );
 
--- src/components/custom-button/custom-button.component.jsx
+++ src/components/custom-button/custom-button.component.jsx
@@ -2,9 +2,16 @@ import React from "react";
 
 import "./custom-button.styles.scss";
 
-const CustomButton = ({ children, isGoogleSignIn, ...otherProperties }) => (
+const CustomButton = ({
+  children,
+  isGoogleSignIn,
+  inverted,
+  ...otherProperties
+}) => (
   <button
-    className={`${isGoogleSignIn ? "google-sign-in" : ""} custom-button`}
+    className={`${inverted ? "inverted" : ""} ${
+      isGoogleSignIn ? "google-sign-in" : ""
+    } custom-button`}
     {...otherProperties}
   >
     {children}
