601cdfaaf315a493760da8750cfdcd1dca768a10
akepativ <Laddu1988>
2020-04-09 19:23:05 +0100

    added cart Reducer and adding items to checkout

--- src/redux/cart/cart.actions.js
+++ src/redux/cart/cart.actions.js
@@ -1,5 +1,10 @@
 import CartActionType from "./cart.types";
 
 export const toggleCartHidden = () => ({
-  type: CartActionType.TOGGLE_CART_HIDDEN
+  type: CartActionType.TOGGLE_CART_HIDDEN,
+});
+
+export const addItem = (item) => ({
+  type: CartActionType.ADD_ITEM,
+  payload: item,
 });
--- src/redux/cart/cart.reducer.js
+++ src/redux/cart/cart.reducer.js
@@ -1,7 +1,9 @@
 import CartActionType from "./cart.types";
+import { addItemsToCart } from "./cart.utils";
 
 const INTITIAL_STATE = {
-  hidden: true
+  hidden: true,
+  cartItems: [],
 };
 
 const CartReducer = (state = INTITIAL_STATE, action) => {
@@ -9,7 +11,12 @@ const CartReducer = (state = INTITIAL_STATE, action) => {
     case CartActionType.TOGGLE_CART_HIDDEN:
       return {
         ...state,
-        hidden: !state.hidden
+        hidden: !state.hidden,
+      };
+    case CartActionType.ADD_ITEM:
+      return {
+        ...state,
+        cartItems: addItemsToCart(state.cartItems, action.payload),
       };
     default:
       return state;
